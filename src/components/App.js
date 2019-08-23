51982d8d8dac4f525fd2ddd5b4668f0559913721
hujimori <hujimori16@gmail.com>
2019-08-23 16:23:23 +0900

    tutorial終了

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Fotter />
-  </div>
+    <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+    </div>
 )
 
 export default App
